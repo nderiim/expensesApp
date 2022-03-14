0b12df2704cf28873f6d78fae47baa54d64b9ab8
Nderim <bunjaku.nderim@hotmail.com>
2022-03-14 23:16:00 +0100

    App set up

--- src/screens/AddScreen.js
+++ src/screens/AddScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { View, TextInput, StyleSheet, TouchableOpacity, FlatList, SafeAreaView, ActivityIndicator } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Button, SafeAreaView, ActivityIndicator } from 'react-native'
 
 const AddScreen = ({ navigation }) => {
     const dispatch = useDispatch();
@@ -11,39 +11,38 @@ const AddScreen = ({ navigation }) => {
     return (
     <>
         <SafeAreaView style={{ height: '100%'}}>
-                {
-                    searchResult.length == 0 && showActivityIndicator && keyword.length != 0
-                    ? <ActivityIndicator size={'large'} color={'lightgrey'} style={{flex:1}}/>
-                    :<FlatList
-                        style={{ alignSelf: 'center' }}
-                        numColumns={'2'}
-                        showsVerticalScrollIndicator={false}
-                        keyExtractor={() => Math.random() * 10}
-                        data={searchResult}
-                        renderItem={({item}) => 
-                            <MovieCard
-                                navigation={navigation}
-                                id={item.id && item.id}
-                                title={item.title ? item.title : item.name}
-                                imageUri={/undefined$/.test(item.image) ? 'https://www.wildhareboca.com/wp-content/uploads/sites/310/2018/03/image-not-available.jpg' : item.image}
-                                genre={item.genre && item.genre}
-                                released={item.release_date && item.release_date}
-                                type={item.type && item.type}
-                                description={item.overview && item.overview}
-                                imdbID={item.imdbID && item.imdbID}
-                                imdbRating={item.vote_average && item.vote_average}
-                                video={item.video && item.video}
-                                cast={item.cast && item.cast}
-                                media_type={item.media_type}
-                            />
-                        }
-                    />
-                }
+            <View style={{ marginHorizontal: 10, marginTop: 20}}>
+                <Text style={styles.textHeader}>Shpenzimi</Text>
+                <TextInput
+                    style={styles.textInput}
+                />
+
+                <Text style={styles.textHeader}>Cmimi</Text>
+                <TextInput
+                    style={styles.textInput}
+                />
+
+                <Text style={styles.textHeader}>Data</Text>
+                
+                <Button title='Shto' onPress={() => console.log('Shto')}/>
+            </View>
         </SafeAreaView>
     </>
     )
 }
 
+const styles = StyleSheet.create({
+    textHeader: {
+        fontSize: 26,
+        marginBottom: 10
+    },
+    textInput:{
+        borderWidth: 1, 
+        borderColor: 'black', 
+        padding: 10,        
+        marginBottom: 10
+    }
+})
 export default AddScreen
 
 
